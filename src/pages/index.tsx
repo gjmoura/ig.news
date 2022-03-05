ac130f7eb68b2a427e9d2c60429a18c53959e132
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2022-03-05 00:22:40 -0300

    Finaliza estilização da página inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -11,12 +12,13 @@ export default function Home() {
 
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <spam>👏 Hey, Welcome</spam>
+          <span>👏 Hey, Welcome</span>
           <h1>News about <br /> the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
