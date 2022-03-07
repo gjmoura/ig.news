858d11d3e3e51cc64444611dadccfcb0f3ee667c
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2022-03-07 13:46:41 -0300

    Implementa sistema de autenticação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  console.log(session);
+
+  return session ? (
+    <button type="button" onClick={() => signOut()} className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Gustavo Jerônimo
+      <>{session.user.name}</>
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" onClick={() => signIn('github')} className={styles.signInButton}>
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
