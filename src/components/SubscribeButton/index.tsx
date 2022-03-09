8174d7d3408206bc5e34df3f7ad16cec1f10f73c
Gustavo Jerônimo <gustavo.jeronimo@hotmail.com>
2022-03-08 22:35:26 -0300

    Implementa pagamento com Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,41 @@
-import styles from './styles.module.scss';
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-  priceId: string,
+  priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  return(
-    <button type="button" className={styles.subscribeButton}>Subscribe now</button>
-  )
+  const session = useSession();
+
+  const handleSubscribe = async () => {
+    if (session.status !== "authenticated") {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+      
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  );
 }
